6f8f9a012b40ec17fe2c26fcdde99e4a2ea49481
Matt Atkinson <matkinson01@gmail.com>
2023-02-25 12:04:37 -0700

    custom actions events added

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,19 @@
-<header>
+<script lang="ts">
+    import { click_outside } from "$lib/click_outside";
+
+    let header_element: HTMLElement | undefined;
+    $: console.log('header_element', header_element);
+
+    function click(e: CustomEvent) {
+        console.log('e', e);
+    }
+
+
+</script>
+
+<header use:click_outside on:click-outside={click} bind:this={header_element}>
     <h1><a href="/">Syntax Podcast</a></h1>
+    <input type="text" >
 </header>
 
 <style>
